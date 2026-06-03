3eb6248f2c46b8740d99a40d1b09e734765ca988
Nadeesha Madhushan <nadeeshamadhushan9@gmail.com>
2026-06-04 00:43:39 +0530

    add health endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+
+    r := gin.Default()
+
+    r.GET("/health", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "status": "UP",
+        })
+    })
+
+    r.Run(":8080")
+}
